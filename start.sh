3c19b207645b2020082e08d8e30be98a3c7c2ec6
Brian Ford <btford@umich.edu>
2014-04-29 14:19:37 -0700

    fix: add sub_filter

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+HOST="npm.whatever.com"
+
 # normalize the working dir to the directory of the script
 cd $(dirname $0);SCRIPT_DIR=$(pwd)
 
 # interpolate pwd into the template
-sed "s#{{pwd}}#`pwd`#g" nginx.template.conf > nginx.conf
+sed -e "s#{{pwd}}#`pwd`#g" \
+    -e "s#{{host}}#$HOST#g" \
+    nginx.template.conf > nginx.conf
 
 # start nginx
-nginx -c `pwd`/nginx.conf
+#nginx -c `pwd`/nginx.conf
