1134bf4b1286af830e834d1a2aef1cadbf7a53ba
Brian Ford <btford@umich.edu>
2014-05-13 19:57:30 -0700

    i am bad at computers so srry

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd $(dirname $0);SCRIPT_DIR=$(pwd)
 wget http://nginx.org/download/nginx-1.6.0.tar.gz
 tar -xvzf nginx-1.6.0.tar.gz
 
-#git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module.git
+git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module.git
 
 
 cd nginx-1.6.0/
